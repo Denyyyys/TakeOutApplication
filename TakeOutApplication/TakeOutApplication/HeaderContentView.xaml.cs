4f8a0331b933bf1d5a4acb63d4ee9969aa55b4a6
dfokashc <dfokashc@pw.edu.pl>
2023-12-26 18:22:33 +0100

    first commit

---
+++ TakeOutApplication/TakeOutApplication/HeaderContentView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TakeOutApplication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeaderContentView : ContentView
+    {
+        public HeaderContentView()
+        {
+            InitializeComponent();
+
+            var assembly = typeof(HeaderContentView);
+
+            gradientImage.Source = ImageSource.FromResource("TakeOutApplication.Assets.Images.gradient.jpg", assembly);
+            userImage.Source = ImageSource.FromResource("TakeOutApplication.Assets.Images.user.png", assembly);
+        }
+    }
+}
