000e20acc02c8d1bb471767f952e9acafbba56cc
Denyyyys <denys.fokashchuk@gmail.com>
2024-01-14 01:44:18 +0100

    idk

--- TakeOutApplication/TakeOutApplication/ProfilePage.xaml.cs
+++ TakeOutApplication/TakeOutApplication/ProfilePage.xaml.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,11 +16,24 @@ namespace TakeOutApplication
         public ProfilePage()
         {
             InitializeComponent();
+            Color myColor = Color.FromRgb(255, 0, 0);
+            string email = "MyEmail@gmail.com";
+            float balance = 100.2f;
+            string favouriteRestaurant = "Central";
+            string favouriteFood = "Almas caviar";
+            emailLabel.Text = email;
+            balanceLabel.Text = $"${balance}";
+            favouriteRestaurantLabel.Text = favouriteRestaurant;
+            favouriteFoodLabel.Text = favouriteFood;
+            bool haveOrders = false;
+
+            string valuee = "sometext";
         }
 
         private async void ToolbarItem_Clicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//SignInPage"); 
         }
+
     }
 }
